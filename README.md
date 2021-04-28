c5878fc9f13b71dea13b2999bbec6dca96ad0992
SC <serg.chaplya@gmail.com>
2021-04-29 02:43:39 +0300

    upd

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chiascripts
+
+wget -o https://raw.githubusercontent.com/iwex/chiascripts/main/init.sh | bash -s
---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update
+apt upgrade -y
+apt install git software-properties-common -y
+add-apt-repository -y ppa:jgmath2000/et
+apt update
+apt install et -y
+
+git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
+cd chia-blockchain
+
+chmod +x ./install.sh
+sh install.sh
