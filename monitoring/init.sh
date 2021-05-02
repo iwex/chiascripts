35013ec1e4c8aa383d8abb47e028aeffd7d378a7
SC <serg.chaplya@gmail.com>
2021-05-02 20:06:37 +0300

    add monitoring

--- monitoring/init.sh
+++ monitoring/init.sh
@@ -5,11 +5,11 @@ rm /root/ChiaMonitor/mtail
 
 wget -c https://github.com/google/mtail/releases/download/v3.0.0-rc45/mtail_3.0.0-rc45_Linux_x86_64.tar.gz -O - | tar -xz -C /root/ChiaMonitor/
 
-wget -O /etc/systemd/system/watch-farmer.service https://raw.githubusercontent.com/iwex/chiascripts/monitoring/watch-farmer.service
-wget -O /etc/systemd/system/watch-farmer.timer https://raw.githubusercontent.com/iwex/chiascripts/monitoring/watch-farmer.timer
-wget -O /etc/systemd/system/mtail.service https://raw.githubusercontent.com/iwex/chiascripts/monitoring/mtail.service
+wget -O /etc/systemd/system/watch-farmer.service https://raw.githubusercontent.com/iwex/chiascripts/monitoring/main/watch-farmer.service
+wget -O /etc/systemd/system/watch-farmer.timer https://raw.githubusercontent.com/iwex/chiascripts/monitoring/main/watch-farmer.timer
+wget -O /etc/systemd/system/mtail.service https://raw.githubusercontent.com/iwex/chiascripts/monitoring/main/mtail.service
 
-wget -O /root/watch-farmer.sh https://raw.githubusercontent.com/iwex/chiascripts/monitoring/watch-farmer.sh
+wget -O /root/watch-farmer.sh https://raw.githubusercontent.com/iwex/chiascripts/main/monitoring/watch-farmer.sh
 chmod +x /root/watch-farmer.sh
 
 systemctl daemon-reload
