93f21669a47ee7ac86d2cf5b71eddf9ece88384a
SC <serg.chaplya@gmail.com>
2021-05-08 23:33:25 +0300

    add harvester autostart

---
+++ harvest/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O /etc/systemd/system/start-harvester.service https://raw.githubusercontent.com/iwex/chiascripts/main/harvest/start-harvester.service
+systemctl daemon-reload
+systemctl enable start-harvester
+systemctl start start-harvester
