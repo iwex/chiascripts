4d69d845b486964f030c7f6e16409f13867bf8a7
SC <serg.chaplya@gmail.com>
2021-05-22 03:27:59 +0300

    add node exporter

---
+++ node_exporter/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm /root/node_exporter
+
+wget -c wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz -O - | tar -xz -C /root/node_exporter/
+
+wget -O /etc/systemd/system/node_exporter.service.service https://raw.githubusercontent.com/iwex/chiascripts/main/node_exporter/node_exporter.service
+
+systemctl daemon-reload
+
+systemctl enable node_exporter
+systemctl start node_exporter
